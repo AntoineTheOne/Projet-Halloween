b77bac9b38b5dc007fbe61ca0fce54684a9a434b
AntoineTheOne <Antoine.Denis2@outlook.com>
2024-10-29 10:13:08 -0400

    Personnage Finit

--- Assets/ScriptPersonnage/AnimationMains.cs
+++ Assets/ScriptPersonnage/AnimationMains.cs
@@ -1,12 +1,36 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-
-
-
 [RequireComponent(typeof(Animator))]
-public class AnimationMains : MonoBehaviour
-{
-    [SerializeField]
 
+public class AnimationHandController : MonoBehaviour
+{
+    [SerializeField] private InputActionReference gripInputActionReference;
+    [SerializeField] private InputActionReference triggerInputActionReference;
+
+    private Animator _handAnimator;
+    private float _gripValue;
+    private float _triggerValue;
+
+    private void Start(){
+        _handAnimator = GetComponent<Animator>();
+    }
+
+
+
+    private void Update() {
+        AnimateGrip();
+        AnimateTrigger();
+    }
+
+
+    private void AnimateGrip(){
+        _gripValue = gripInputActionReference.action.ReadValue<float>();
+        _handAnimator.SetFloat("Grip", _gripValue);
+    }
+
+    private void AnimateTrigger(){
+        _triggerValue = triggerInputActionReference.action.ReadValue<float>();
+        _handAnimator.SetFloat("Trigger", _triggerValue);
+    }
 
 }
